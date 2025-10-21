71bfafec86fddb37a0ac47891ff053b10eb0d299
Edward England <edward@edwebs.net>
2025-10-20 22:05:06 -0500

    Create MenuItem struct for our data

---
+++ Menu/MenuItem.swift
@@ -0,0 +1,14 @@
+//
+//  MenuItem.swift
+//  Menu
+//
+//  Created by Edward England on 10/20/25.
+//
+
+import Foundation
+
+struct MenuItem {
+    var name: String
+    var price: String
+    var imageName: String
+}
