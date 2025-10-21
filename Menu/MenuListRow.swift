e341c76f070c0ac340b007d8b4adf32e486348cc
Edward England <edward@edwebs.net>
2025-10-20 23:58:21 -0500

    Extract row display to MenuListRow view file

---
+++ Menu/MenuListRow.swift
@@ -0,0 +1,40 @@
+//
+//  TestView.swift
+//  Menu
+//
+//  Created by Edward England on 10/20/25.
+//
+
+import SwiftUI
+
+struct MenuListRow: View {
+    
+    var item: MenuItem
+    
+    var body: some View {
+        HStack {
+            
+            Image(item.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 50)
+                .cornerRadius(10)
+            
+            Text(item.name)
+                .bold()
+            
+            Spacer()
+            
+            Text("$" + item.price)
+        }
+        .listRowSeparator(.hidden)
+        .listRowBackground(
+            Color(.brown)
+                .opacity(0.1)
+        )
+    }
+}
+
+#Preview {
+    MenuListRow(item: MenuItem(name: "Test Item", price: "$3.99", imageName: "tacosushi"))
+}
