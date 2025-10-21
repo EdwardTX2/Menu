e17f156bbccaf02ad91102aca719ab96e96d090e
Edward England <edward@edwebs.net>
2025-10-20 21:48:58 -0500

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Edward England on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
